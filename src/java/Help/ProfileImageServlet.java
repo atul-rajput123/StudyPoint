5dce5c5a43110722b319e44552e9996a2b644913
Atul Rajput <155346036+atul-rajput123@users.noreply.github.com>
2025-09-29 18:31:36 +0530

    Update ProfileImageServlet.java

--- src/java/Help/ProfileImageServlet.java
+++ src/java/Help/ProfileImageServlet.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Help;
 
 import java.io.IOException;
@@ -43,7 +39,7 @@ public class ProfileImageServlet extends HttpServlet {
                     }
                 }
             } else {
-                response.sendRedirect("default-placeholder.png"); // Placeholder for missing image
+                response.sendRedirect("default-placeholder.png"); 
             }
         } catch (Exception e) {
             e.printStackTrace();
